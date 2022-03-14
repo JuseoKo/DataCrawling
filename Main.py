262ceab05c4e872e6748c990d6df6e01d5b44a60
maria <mariat1717@daum.net>
2022-03-14 13:16:00 +0900

    Main 비움

--- Main.py
+++ Main.py
@@ -1,11 +1,1 @@
 #메인프로그램
-echo "# Cap3" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/MariaTe1/Cap3.git
-git push -u origin main
-
-
-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
