ed9b9714f22d3a597aa22a164bd571c8f2087055
maria <mariat1717@daum.net>
2022-04-08 22:05:22 +0900

    프로그래머스 추가

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
-import re
-import feedparser
+# import requests
+# from bs4 import BeautifulSoup
+# import re
+# import feedparser
+#
+# url = 'https://programmers.co.kr/job'
+# #url 세팅
+# response = requests.get(url)
+# soup = BeautifulSoup(response.content, 'html.parser')
+# # soup = re.findall('<(.+?)>', str(soup))
+# print(soup)
+# # data = soup.select('#news_body_area')[0].get_text()
+# # data = re.sub('\[(.+?)\]', '',str(data))
+# # data = re.sub('※(.+?)지', '',str(data))
+# # data = re.sub('/(.+?)뉴스1', '',str(data))
 
-url = 'https://programmers.co.kr/job'
-#url 세팅
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-# soup = re.findall('<(.+?)>', str(soup))
-print(soup)
-# data = soup.select('#news_body_area')[0].get_text()
-# data = re.sub('\[(.+?)\]', '',str(data))
-# data = re.sub('※(.+?)지', '',str(data))
-# data = re.sub('/(.+?)뉴스1', '',str(data))
 
-
+for i in range(0, 2):
+    print(i)
--- wanted_Crawler.py
+++ wanted_Crawler.py
@@ -6,15 +6,15 @@ from bs4 import BeautifulSoup
 
 #각종 변수선언
 if True:
+    #크롬드라이버 관련
     #크롬 경로
     options = webdriver.ChromeOptions()
     options.binary_location = "/Users/maria/Desktop/Google Chrome.app/Contents/MacOS/Google Chrome"
-
     #크롬 드라이버 경로
     chrome_driver_binary = "/Users/maria/Desktop/Code/capstone_3/chromedriver"
     driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
 
-    #공통변수선언
+    #공통변수
     file_path = './Data/Work.txt'
     #주소를 담아둘 배열
     url_list = []
@@ -27,15 +27,17 @@ if True:
     #url
     wanted_url = 'https://www.wanted.co.kr/wdlist?country=kr&job_sort=job.latest_order&years=-1&locations=all'
 
-    # 프로그래머스 변수 선언 페이지의 구조
-    #번호 갯수
-    programers_page_num = 2
-    # 프로그래머스 주소
-    programers_url = 'https://programmers.co.kr/job'
+    # 프로그래머스 변수 선언
+    #페이지 수
+    programers_page_num = 3
+    #주소
+    programers_url = 'https://programmers.co.kr/job?page='
+    # 페이지 구조
+    programers_text_value = '/html/body/div[3]/div/div[1]/div/div[1]'
 
 #원티드
-#url불러오기
 def wanted() :
+    #url로드기능
     driver.get(wanted_url)
     time.sleep(3)
     #추출할 웹사이트 갯수를 range로 표현함 , url 로드기능
@@ -56,41 +58,60 @@ def wanted() :
             #더이상 url 데이터가 없으면 루프문 탈출
         except:
             break
-    # 데이터 로드기능
-    for j in range(0, len(url_list)) :
-        #찾은 주소로 들어가기(로딩대기)
-        driver.get(url_list[j])
-        try:
-            #해당 경로에 있는 텍스트 긁어오기
-            in_data = driver.find_element(by=By.XPATH, value=wanted_text_value).text
-        except:
-            #로딩이 제대로 안된채로 긁어오면 에러발생 (재로딩)
-            print('재로딩')
-            driver.get(url_list[j])
-            time.sleep(2)
-            in_data = driver.find_element(by=By.XPATH, value=wanted_text_value).text
-        #in_data 변수에 글자가 들어있으니 원하는 저장포맷으로 저장하기
-        save(in_data, url_list)
+    #데이터 로드기능
+    load(wanted_text_value)
     #리스트 초기화
     url_list.clear()
 
 #프로그래머스
-#url불러오기
 def programers():
-    # programers_page = ''
-    driver.get(programers_url+programers_page)
-    time.sleep(5)
-    for i in range(0, programers_page_num*20):
+    #url로드기능
+    #페이지 설정
+    count_page = 0
+    for j in range(0, programers_page_num) :
+        #url설정 , 접속
+        count_page = count_page + 1
+        programers_url_set = programers_url + str(count_page)
+        driver.get(programers_url_set)
+        time.sleep(5)
+
+        for i in range(0, programers_page_num*20):
+            try:
+                count = str(1+i)
+                path = '//*[@id="list-positions-wrapper"]/ul/li['+count+']/div[2]/h5/a'
+                url_data = driver.find_element(by=By.XPATH,value=path).get_attribute('href')
+                url_list.append(url_data)
+            except:
+                if programers_page_num >= count_page :
+                    continue
+                else:
+                    break
+
+    print(f'주소 : {url_list}\n 갯수 : {len(url_list)}')
+    load(programers_text_value)
+    #리스트 초기화
+    url_list.clear()
+
+#데이터 로드기능
+def load(value):
+    # 데이터 로드기능
+    data_value = value
+    for k in range(0, len(url_list)) :
+        #찾은 주소로 들어가기(로딩대기)
+        driver.get(url_list[k])
         try:
-            count = str(1+i)
-            path = '//*[@id="list-positions-wrapper"]/ul/li['+count+']/div[2]/h5/a'
-            url_data = driver.find_element(by=By.XPATH,value=path).get_attribute('href')
-            url_list.append(url_data)
+            #해당 경로에 있는 텍스트 긁어오기
+            in_data = driver.find_element(by=By.XPATH, value=data_value).text
         except:
-            for j in range(0, programers_page_num):
-                programers_page = '/job?page='+j
-            # break
-    print(url_list)
+            #로딩이 제대로 안된채로 긁어오면 에러발생 (재로딩)
+            print('재로딩')
+            driver.get(url_list[k])
+            time.sleep(2)
+            in_data = driver.find_element(by=By.XPATH, value=data_value).text
+        #in_data 변수에 글자가 들어있으니 원하는 저장포맷으로 저장하기
+        save(in_data, url_list)
+    #리스트 초기화
+    url_list.clear()
 
 #저장
 def save(data, url_len):
@@ -100,7 +121,7 @@ def save(data, url_len):
 #메인
 if __name__ == '__main__' :
     # 원티드 채용 사이트
-    # wanted()
+    wanted()
     # #프로그래머스 채용 사이트
     programers()
 
