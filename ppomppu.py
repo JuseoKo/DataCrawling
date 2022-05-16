c01b4a4b6ab33b5a21d55869ff9ae8418b040146
maria <mariat1717@daum.net>
2022-05-16 13:51:03 +0900

    뽐뿌추가

---
+++ ppomppu.py
@@ -0,0 +1,103 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+import re
+
+if True:
+    #크롬드라이버 관련
+    #크롬 경로
+    options = webdriver.ChromeOptions()
+    options.binary_location = "/Users/maria/Desktop/Google Chrome.app/Contents/MacOS/Google Chrome"
+    #크롬 드라이버 경로
+    chrome_driver_binary = "/Users/maria/Desktop/Code/capstone_3/chromedriver"
+    driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
+
+    #url 번호카운트 , 데이터 xpath,날짜 데이터 xpath, 데이터 프레임 생성, 데이터 저장경로,
+    line = 1
+    value = '/html/body/div/div[2]/div[5]/div/table[5]/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr/td'
+    value_2 = '/html/body/div/div[2]/div[5]/div/table[2]/tbody/tr[3]/td/table/tbody/tr/td[5]/div/div[1]'
+    frame : ''
+    file_path = './New/ppomppu.csv'
+    #데이터 저장 포맷 설정
+
+
+#메인
+def main():
+    for i in range(line, 25000):
+        try:
+            url = 'https://www.ppomppu.co.kr/zboard/view.php?id=developer&page=1&divpage=5&no='+str(i)+''
+            driver.get(url)
+            data = driver.find_element(by=By.XPATH, value=value).text
+            data = preprocessing(data)
+            day = driver.find_element(by=By.XPATH, value=value_2).text
+            day = re.search('등록일: (.+?) ', day)
+            day = day.group(1)
+            years = year(day)
+            months = month(day)
+            if data == '':
+                print(f'{i}번째 데이터 없음')
+                continue
+            else:
+                print(f'{i}번째 성공')
+                print(f'데이터 번호 : {i}, 내용 : {data}')
+                save(data, years, months, i)
+                frame.to_csv(file_path, encoding='utf-8-sig')
+            if i%3000 == 0:
+                frame.to_csv(file_path, encoding='utf-8-sig')
+                print('저장')
+        except :
+            continue
+
+
+#년 계산
+def year(day_data):
+    year_data = re.sub('-(.+?)*', '', day_data)
+    return year_data
+#월계산
+def month(day_data):
+    month_data = re.search('-(.+?)-', day_data)
+    month_data = month_data.group(1)
+    return month_data
+#전처리
+def preprocessing(data):
+    data_def = re.sub('<(.+?)>', '',str(data))
+    data_def = re.sub('\r', '', str(data_def))
+    data_def = re.sub('\t', '', str(data_def))
+    data_def = re.sub('\n', '', str(data_def))
+    data_def = re.sub('\f', '', str(data_def))
+    data_def = re.sub('\v', '', str(data_def))
+    data_def = re.sub('\[', '', str(data_def))
+    data_def = re.sub('\]', '', str(data_def))
+    data_def = data_def.strip()
+    return data_def
+
+#저장
+def save(in_data, year, month, add_line):
+    global j, frame
+    #데이터 추가
+    frame.loc[add_line, 'text'] = in_data
+    #년도 추가
+    frame.loc[add_line, 'year'] = str(year)
+    frame.loc[add_line, 'month'] = str(month)
+
+#데이터프레임(csv) 불러오기
+def dataframe():
+    global frame
+    try:
+        frame = pd.read_csv(file_path, index_col = 0)
+        frame = frame.astype({'year': 'str', 'month': 'str'})
+    except:
+        data = {
+            'text' : [],
+            'year' : [],
+            'month' : []
+        }
+        frame = pd.DataFrame(data)
+        frame.to_csv(file_path, encoding='utf-8-sig')
+    return frame
+if __name__ == '__main__':
+    dataframe()
+    main()
+    frame.to_csv(file_path, encoding='utf-8-sig')
+    print(frame)
+
--- test.py
+++ test.py
@@ -1,10 +1,47 @@
+from bs4 import BeautifulSoup
+import re
+import time
+from selenium import webdriver
+import feedparser
+import pandas as pd
+import datetime
+import pandas as pd
+
+import re
+def preprocessing(data):
+    data_def = re.sub('<(.+?)>', '',str(data))
+    data_def = re.sub('\r', '', str(data_def))
+    data_def = re.sub('\t', '', str(data_def))
+    data_def = re.sub('\n', '', str(data_def))
+    data_def = re.sub('\f', '', str(data_def))
+    data_def = re.sub('\v', '', str(data_def))
+    data_def = re.sub('\[', '', str(data_def))
+    data_def = re.sub('\]', '', str(data_def))
+    data_def = data_def.strip()
+    return data_def
+
+# #193
+# # url = 'https://gall.dcinside.com/board/view/?id=programming&no=165001'
+# url = 'https://www.ppomppu.co.kr/zboard/view.php?id=developer&page=1&divpage=5&no=2'
+# urls = 'https://kldp.org'
+# red = requests.get(url)
+# soup = BeautifulSoup(red.content, 'html.parser').text
+# # data = soup.select('body > div > div.contents > div.container > div')
+# # data = soup.select('body > div > div.contents > div.container > div > table:nth-child(15) > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td')
+# # data = urls+data
+# # data = preprocessing(data)
+# print(soup)
+# import pyautogui as p
+# # p.press()
+
 # from bs4 import BeautifulSoup
-# import re
-# import time
+#
 # from selenium import webdriver
+# from selenium.webdriver.common.by import By
 # import feedparser
 # import pandas as pd
 # import datetime
+#
 # #각종 변수선언
 # if True:
 #     #크롬드라이버 관련
@@ -15,51 +52,31 @@
 #     chrome_driver_binary = "/Users/maria/Desktop/Code/capstone_3/chromedriver"
 #     driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
 #
-#     url = 'https://www.ppomppu.co.kr/zboard/zboard.php?id=developer&page=1&category=1&divpage=5'
-#     url_add = 'https://www.ppomppu.co.kr/zboard/'
-#
-#     driver.get(url)
-#     driver.implicitly_wait(10)
-#     htmls = driver.page_source
-#     soup = BeautifulSoup(htmls, 'html.parser')
-#     href = soup.select('#revolution_main_table > tbody')
-#     href = re.findall('href="(.+?)">', str(href))
-#     href = url_add+href[3]
-#     # href = set(href)
-#     # href = list(href)
-#     print(href)
-# #https://www.ppomppu.co.kr/zboard/view.php?id=developer&page=1&divpage=5&category=1&no=25322
-# #https://www.ppomppu.co.kr/zboard/view.php?id=developer&amp;page=1&amp;divpage=5&amp;category=1&amp;no=25322
-
-a, b = map(int, input().split())
-print(a+b)
-# from bs4 import BeautifulSoup
-# import requests
-# import re
-# def preprocessing(data):
-#     data_def = re.sub('<(.+?)>', '',str(data))
-#     data_def = re.sub('\r', '', str(data_def))
-#     data_def = re.sub('\t', '', str(data_def))
-#     data_def = re.sub('\n', '', str(data_def))
-#     data_def = re.sub('\f', '', str(data_def))
-#     data_def = re.sub('\v', '', str(data_def))
-#     data_def = re.sub('\[', '', str(data_def))
-#     data_def = re.sub('\]', '', str(data_def))
-#     data_def = data_def.strip()
-#     return data_def
+#     #URL 과 RSS 데이터를 몇개 받아올지 갯수
+#     url = 'https://www.ppomppu.co.kr/zboard/view.php?id=developer&page=1&divpage=5&no=10000'
+#     value = '/html/body/div/div[2]/div[5]/div/table[5]/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr/td'
+#     value_2 = '/html/body/div/div[2]/div[5]/div/table[2]/tbody/tr[3]/td/table/tbody/tr/td[5]/div/div[1]'
+# driver.get(url)
+# data = driver.find_element(by=By.XPATH, value=value).text
+# data = preprocessing(data)
+# day = driver.find_element(by=By.XPATH, value=value_2).text
+# day = re.search('등록일: (.+?) ', day)
+# day = day.group(1)
+# #년 계산
+# def year(day_data):
+#     year_data = re.sub('-(.+?)*', '', day_data)
+#     return year_data
+# #월계산
 # def month(day_data):
 #     month_data = re.search('-(.+?)-', day_data)
 #     month_data = month_data.group(1)
 #     return month_data
 #
-# #193
-# url = 'https://www.ppomppu.co.kr/zboard/zboard.php?id=developer&page=1&category=1&divpage=5'
-# urls = 'https://kldp.org'
-# red = requests.get(url)
-# soup = BeautifulSoup(red.content, 'html.parser')#revolution_main_table > tbody > tr:nth-child(6) > td:nth-child(3)#revolution_main_table > tbody > tr:nth-child(6) > td:nth-child(3) > a')
-# # data = urls+data
-# # data = preprocessing(data)
-# print(data)
-# import pyautogui as p
-# # p.press()
-#
+# print(year(day))
+# print(month(day))
+
+# month(data_2)
+o = pd.read_csv('./New/okky_Data.csv', index_col=0)
+p = pd.read_csv('./New/ppomppu.csv', index_col=0)
+# n = pd.read_csv('./New/News_Data.csv', index_col=0)
+print(o)
